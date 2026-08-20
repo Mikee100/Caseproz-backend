cec3728be8fc8968ebeed8b8222dbb2cccadb49e
Mikee100 <99830445+Mikee100@users.noreply.github.com>
2026-08-20 12:55:35 +0300

    feat: change default server port from 4000 to 7000

--- server.js
+++ server.js
@@ -85,6 +85,6 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
